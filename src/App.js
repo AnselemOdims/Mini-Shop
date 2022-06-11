eb20d074d8d40f77761847dde32b1fd9e6ea723b
Anselem <anselemodims@gmail.com>
2022-06-11 19:30:05 +0100

    Chore: configure eslint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  );
+  render() {
+
+    return (
+      <div className="App">
+        <h1>React App</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
