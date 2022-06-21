7c56170ba41042aeb8da5515b0e3f7e72edf8795
Anselem <anselemodims@gmail.com>
2022-06-21 19:07:57 +0100

    Feat: build the details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,6 +1,106 @@
 import { Component } from 'react';
 import { withRouter } from 'react-router-class-tools';
+import styled from 'styled-components';
+import Image from '../assets/images/Image.png';
 
+const StyledDetails = styled.section`
+  padding: 9.5rem 15.21% 11.9375rem 6.72%;
+  display: flex;
+
+  > div {
+    &:nth-of-type(1),
+    &:nth-of-type(3) {
+      display: flex;
+      flex-direction: column
+    }
+
+    &:nth-of-type(1) {
+      margin-right: 2.5rem;
+      width: 7.03%;
+      gap: 2.03rem;
+
+      img {
+        width: 5rem;
+        height: 5rem;
+      }
+    }
+
+    &:nth-of-type(2) {
+      margin-right: 5.25rem;
+      img {
+        width: 38.125rem;
+        height: 31.9375rem
+      }
+    }
+
+    &:nth-of-type(3) {
+      color: var(--text-color-1);
+      
+      > p {
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 159.96%;
+      }
+
+      h2 {
+        font-weight: 600;
+        font-size: 30px;
+        line-height: 27px;
+        margin-bottom: 1rem;
+      }
+
+      h3 {
+        font-weight: 400;
+        font-size: 30px;
+        line-height: 27px; 
+        margin-bottom: 2.6875rem 
+      }
+
+      > div {
+        p {
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 18px;
+        margin-bottom: 0.5rem;
+      }
+
+      &:nth-of-type(1) {
+        margin-bottom: 1.5rem;
+      }
+
+      &:nth-of-type(2) {
+        margin-bottom: 2.25rem;
+      }
+
+      &:nth-of-type(3) {
+        margin-bottom: 1.25rem;
+
+        p {
+          margin-bottom: 0.625rem;
+        }
+        span {
+          font-weight: 700;
+          font-size: 24px;
+          line-height: 18px;
+        }
+      }
+
+      &:nth-of-type(4) {
+        margin-bottom: 2.5rem;
+
+          button {
+            width: 100%;
+            padding: 1rem 2rem;
+            border: none;
+            color: var(--white);
+            background: var(--primary-color)
+          }
+      }
+
+    }
+    }
+  }
+`;
 class Details extends Component {
   componentDidMount() {
     // const { id } = this.props.match.params;
@@ -9,17 +109,49 @@ class Details extends Component {
 
   render() {
     return (
-      <div>
-        <h1>This is the details page</h1>
-        <h1>This is the details page</h1>
-        <h1>This is the details page</h1>
-        <h1>This is the details page</h1>
-        <h1>This is the details page</h1>
-        <h1>This is the details page</h1>
-        <h1>This is the details page</h1>
-        <h1>This is the details page</h1>
-        <h1>This is the details page</h1>
-      </div>
+      <StyledDetails>
+        <div>
+          <img src={Image} alt="product" />
+          <img src={Image} alt="product" />
+          <img src={Image} alt="product" />
+        </div>
+        <div>
+          <img src={Image} alt="product" />
+        </div>
+        <div>
+          <h2>Apollo</h2>
+          <h3>Running Short</h3>
+          <div>
+            <p>SIZE: </p>
+            <div>
+              <button type="button">XS</button>
+              <button type="button">S</button>
+              <button type="button">M</button>
+              <button type="button">L</button>
+            </div>
+          </div>
+          <div>
+            <p>COLOR: </p>
+            <div>
+              <button type="button">X</button>
+              <button type="button">S</button>
+              <button type="button">M</button>
+            </div>
+          </div>
+          <div>
+            <p>PRICE: </p>
+            <span>$50.00</span>
+          </div>
+          <div>
+            <button type="button">ADD TO CART</button>
+          </div>
+          <p>
+            Find stunning women&#39;s cocktail dresses and party dresses. Stand out
+            in lace and metallic cocktail dresses and party dresses from all
+            your favorite brands.
+          </p>
+        </div>
+      </StyledDetails>
     );
   }
 }
