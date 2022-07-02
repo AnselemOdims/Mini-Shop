63108e37df672f470ca9f580af51891d7053b555
Anselem <anselemodims@gmail.com>
2022-07-02 01:14:22 +0100

    Feat: add function to increase if item aleady available

--- src/redux/cart/reducers/cart.js
+++ src/redux/cart/reducers/cart.js
@@ -3,23 +3,31 @@ import * as Actions from '../actions/cartAction';
 // declare the initial state
 const initialState = { cart: [] };
 
-const increment = (state, payload) => state.map((item) => {
+const increment = (cart, payload) => cart.map((item) => {
   if (item.id === payload) {
     return {
       ...item,
       qty: item.qty + 1,
     };
   }
-  return {
-    ...item,
-  };
+  return { ...item };
 });
+
+const checkAdd = (cart, payload) => {
+  const addedItem = cart.find((item) => item.id === payload.id);
+  const filteredItems = cart.filter((item) => item.id !== payload.id);
+  if (addedItem) {
+    addedItem.qty += 1;
+    return [...filteredItems, addedItem];
+  }
+  return [...cart, payload];
+};
 
 // the reducer function for the cart
 const cartReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case Actions.CART_ADDED:
-      return { cart: [...state.cart, payload] };
+      return { cart: checkAdd(state.cart, payload) };
     case Actions.QUANTITY_INCREMENTED:
       return { cart: increment(state.cart, payload) };
     default:
